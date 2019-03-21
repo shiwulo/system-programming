35c957bb2b2edafbddbd380536c63e02a98c02fd
shiwulo <38451721+shiwulo@users.noreply.github.com>
2019-03-21 11:15:21 +0800

    Add files via upload

--- ch05/append+fseek.c
+++ ch05/append+fseek.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 int main() {
 	FILE *stream;
-	char buf[100];
+	char buf[10000];
 	int ret;
 	stream = fopen("./system-programming.txt", "a+");
-	ret = fseek(stream, 200, SEEK_SET);
-	printf("\nreturn value of fseek(stream, 200)= %d\n", ret);
-	printf("file position = %ld\n", ftell(stream));
-	fread(buf, 100, 1, stream);
-	printf(buf);
+	ret = fseek(stream, 10, SEEK_SET);
+	printf("\nreturn value of fseek(stream, 10)= %d\n", ret);
+	printf("file position after fseek(10) = %ld\n", ftell(stream));
+	memset(buf, 0, 10000);
+	int nItem = fread(buf, 26, 1, stream);
+	printf("%s\n", buf);
+	printf("file position after fread() = %ld\n", ftell(stream));
+	getchar();
 	fprintf(stream, "append?\n");
 	printf("\nfile position = %ld\n", ftell(stream));
 	return 0;
