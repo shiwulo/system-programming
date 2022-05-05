aa3c6e691bbe2843148d08f762fa15b1c00b3651
shiwulo <shiwulo@gmail.com>
2022-05-05 22:18:29 +0800

    註解註解

--- ch10/testStack2.c
+++ ch10/testStack2.c
@@ -16,6 +16,14 @@ void seg_fault(int sigNo) {
 	while(1);
 }
 
+/**
+ * @brief 這個程式碼是不斷的修改SP（stack pointer），藉此讓堆疊變大，Linux預設stack最大只有8MB
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
+
 int main(int argc, char** argv) {
 	char base;
 	char *stackP=&base;
@@ -37,6 +45,10 @@ int main(int argc, char** argv) {
 		(int)rlim.rlim_max/1024);
 	printf("pid = %d", getpid());
 	getchar();
+	// 在底下這個while loop中不斷的 rsp 暫存器 減去 (4096-8)
+	// 因為堆疊是由上往下長，因此相當於堆疊不斷的往下長
+	// 底下有二種寫法，分別使用組合語言，另一個則是使用一個區域變數的address
+	// 把local variable的address當作是堆疊的開始位址，然後往下移動4096 bytes
 	while(1) {
 		if(usingSP) {
 			__asm__(
@@ -46,7 +58,9 @@ int main(int argc, char** argv) {
 				"MOVQ %%rsp, %0;\n"
 				:"=r"(stackP)
 			);
-		} else stackP -= 4096;
+		} else {
+			stackP -= 4096;
+		}
 		*stackP = 1;
 		printf("%ldK\n",(&base - stackP)/(1024));
 	}
--- ch10/test_sig.c
+++ ch10/test_sig.c
@@ -10,12 +10,20 @@ volatile char *c;
 void sighandler(int signumber) {
     printf("get a signal named '%d', '%s'\n", signumber,
            sys_siglist[signumber]);
-    c=malloc(sizeof(char));
+    c=malloc(sizeof(char)); //這一行到底有沒有用，要看編譯器怎樣編譯
 }
+
+/**
+ * @brief 一個很簡單程式，故意存取錯誤的記憶體然後攔截這個錯誤。
+ * 
+ * @param argc 
+ * @param argv 
+ * @return int 
+ */
 
 int main(int argc, char **argv) {
     assert(signal(SIGSEGV, sighandler) != SIG_ERR);  
-    *c = 'a';
+    *c = 'a';   //雖然在signal handler的地方重新給 c 值，但 *c 其實已經放在暫存器（register）中，因此signal handler無法修正這個錯誤
     printf("my pid is %d\n", getpid());
     printf("press any key to resume\n");
     getchar();
