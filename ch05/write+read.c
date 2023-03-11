9127131a25e9e7fa5b4c7c8338cb3d3c71dac4a8
shiwulo <shiwulo@gmail.com>
2023-03-11 16:05:30 +0800

    Update write+read.c

--- ch05/write+read.c
+++ ch05/write+read.c
@@ -1,8 +1,10 @@
+// 這個程式創建了一個名為 tmp 的文件，並在其中寫入一行文字，打開方式由命令行參數指定
 #include <stdio.h>
+
 int main(int argc, char **argv) {
-	FILE* file;
-	file = fopen("./tmp", argv[1]);
-	fprintf(file, "this_is_a_tmp_file\n");;
-	fclose(file);
-	return 0;
+    FILE* file;
+    file = fopen("./tmp", argv[1]);  // 創建一個文件，文件名為 tmp，打開方式由命令行參數指定，並獲取其文件指針
+    fprintf(file, "this_is_a_tmp_file\n");  // 在文件中寫入一行文字
+    fclose(file);  // 關閉文件
+    return 0;
 }
