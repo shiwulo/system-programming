c4c604135e0f8a1295d82057a252398f4a6f9c27
shiwulo <shiwulo@gmail.com>
2023-03-11 15:57:28 +0800

    Update strlen.c

--- ch05/strlen.c
+++ ch05/strlen.c
@@ -1,11 +1,13 @@
+// 這個程式示範了在 C 語言中使用 strlen 函數獲取一個字串的長度
 #include <stdio.h>
 #include <wchar.h>
 #include <string.h>
 
 int main(int argc, char **argv)
 {
-	char *str = "中文"; 
-	printf("%d\n", (int)strlen(str));
-	return 0;
+    char *str = "中文";  // 宣告一個字符指針 str，並賦值為 "中文"
+    printf("%d\n", (int)strlen(str));  // 使用 strlen 函數獲取 str 的長度，並輸出其返回值
+    return 0;
 }
+
 
