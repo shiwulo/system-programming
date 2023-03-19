c6fdf7d6462b866b5341329bded45584c944350d
shiwulo <shiwulo@gmail.com>
2023-03-19 09:42:38 +0800

    Update gettime.c

--- ch06/gettime.c
+++ ch06/gettime.c
@@ -1,14 +1,20 @@
 /*
-gettime.c
-print time
-*/
+ * gettime.c
+ * 功能：顯示當前系統時間
+ * 用法：./program
+ */
 #include <stdio.h>
 #include <time.h>
  
 int main(void)
 {
+    // 獲取當前時間
     time_t t = time(NULL);
+    
+    // 使用 localtime 函數將時間轉換為 struct tm 格式
     struct tm *pTM = localtime(&t);
+    
+    // 獲取年、月、日、時、分、秒等時間元素
     int yyyy = pTM->tm_year + 1900;
     int mm = pTM->tm_mon + 1;
     int dd = pTM->tm_mday;
@@ -16,5 +22,6 @@ int main(void)
     int min = pTM->tm_min;
     int ss = pTM->tm_sec;
      
-    printf("%d-%d-%d-%d:%d:%d\n", yyyy, mm, dd, hh, min, ss);
+    // 輸出格式化後的時間
+    printf("%d-%02d-%02d-%02d:%02d:%02d\n", yyyy, mm, dd, hh, min, ss);
 }
