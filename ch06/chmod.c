4c311bd4e7e7a6f1e20887b2824553b0c95f7545
shiwulo <shiwulo@gmail.com>
2023-03-19 09:38:03 +0800

    Update chmod.c

--- ch06/chmod.c
+++ ch06/chmod.c
@@ -1,15 +1,25 @@
 #include <stdio.h>
 #include <sys/stat.h>
 
+/* 
+ * 功能：設定檔案權限
+ * 用法：./program <檔案名稱> <三位數權限碼>
+ *      其中 <三位數權限碼> 為三位數數字，表示檔案的所有者、群組、其他人權限
+ * 備註：本程式必須以 root 權限執行
+ */
 int main(int argc, char **argv)
 {
-	int perm;
-	int owner, grp, others;
-	sscanf(argv[2], "%1d%1d%1d", &owner, &grp, &others);
-	printf("permission = %d %d %d\n", owner, grp, others);
-	perm = owner<<6 | grp<<3 | others;
-	chmod(argv[1], (mode_t)perm);
+    int perm;
+    int owner, grp, others;
+    
+    // 從命令行參數中讀取要設定權限的檔案名稱和三位數權限碼
+    sscanf(argv[2], "%1d%1d%1d", &owner, &grp, &others);
+    
+    // 將三位數權限碼合併為一個八進制數字
+    perm = owner << 6 | grp << 3 | others;
+    
+    // 使用 chmod 函數設定檔案權限
+    chmod(argv[1], (mode_t)perm);
 	
-	return 0;
+    return 0;
 }
-
