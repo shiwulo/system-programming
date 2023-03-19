924c68f39d012f5d6c39de489a5b1a9a6ff60061
shiwulo <shiwulo@gmail.com>
2023-03-19 09:39:41 +0800

    Update dir.c

--- ch06/dir.c
+++ ch06/dir.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include <dirent.h>
 
+/* 
+ * 功能：列出指定目錄中的所有檔案和子目錄名稱
+ * 用法：./program <目錄名稱>
+ */
 int main(int argc, char **argv)
 {
-	DIR* dir;
-	struct dirent* ent;
-	dir = opendir(argv[1]);
-	ent = readdir(dir);
-	while(ent!=NULL) {
-		printf("%s\n",  ent->d_name);
-		ent = readdir(dir);
-	}
-	closedir(dir);
+    DIR* dir;
+    struct dirent* ent;
+    
+    // 打開目錄
+    dir = opendir(argv[1]);
+    
+    // 讀取目錄中的所有檔案和子目錄
+    ent = readdir(dir);
+    while(ent!=NULL) {
+        printf("%s\n", ent->d_name);
+        ent = readdir(dir);
+    }
+    
+    // 關閉目錄
+    closedir(dir);
 	
-	return 0;
+    return 0;
 }
+
 
