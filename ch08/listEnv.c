ec59f0af23a39439d3af235fea7b126e201c532b
shiwulo <shiwulo@gmail.com>
2025-04-21 11:18:12 +0800

    Update listEnv.c

--- ch08/listEnv.c
+++ ch08/listEnv.c
@@ -2,12 +2,14 @@
 使用方法：
 不用給任何參數
 */
+#define _GNU_SOURCE
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 extern char **environ;
 
 int main(int argc, char**argv) {
-	int i;
+	int i=0;
 	while(environ[i] != NULL) {
 		printf("%s\n", environ[i++]);
 	}
